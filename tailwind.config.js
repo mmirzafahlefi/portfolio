4255d2f08f2157c977348ae66f377ad2def2a69a
mmirzafahlefi <nyobatyuk0910@gmail.com>
2024-01-08 05:40:25 +0700

    update fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     container: {
